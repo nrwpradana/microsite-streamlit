8d3ce05fde5c1f5c7874dadb58da02a89469d5c4
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-31 15:44:02 +0700

    Edit Icon Size

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,17 +11,14 @@ st.header('Nadhiar Ridho Wahyu Pradana')
 
 st.info('Data Enthusiast with an interest in Data Science and Data Visualization')
 
-icon_size = 20
+icon_size = 25
 
-#st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-#st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-#st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
 st_button('github', 'https://github.com/nrwpradana', ' Check my Github', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/nadhiar-ridho-wahyu-pradana/', ' Follow me on LinkedIn', icon_size)
 st_button('newsletter', 'mailto:nadhiar11@gmail.com', ' Mail me', icon_size)
-st_button('projector', 'https://public.tableau.com/app/profile/nadhiar', ' See my Tableau puclic', icon_size)
+st_button('projector', 'https://public.tableau.com/app/profile/nadhiar', ' See my Tableau public', icon_size)
 
-st.info('Check Latest Project Here')
+st.info('Check My Latest Project Here')
 
-st_button('coffee','https://nrwpradana-krisis-pangan-app-nqc42o.streamlitapp.com/','Tetris Capstone Project', icon_size)
-
+st_button('coffee','https://nrwpradana-krisis-pangan-app-nqc42o.streamlitapp.com/',' Tetris Capstone Project', icon_size)
+st_button('projector','https://public.tableau.com/app/profile/nadhiar/viz/PenilaianIntegritasProvinsiJawaBarat/PenilaianIntegritasJabar',' Jabar Dataviz Fest 2022', icon_size)
