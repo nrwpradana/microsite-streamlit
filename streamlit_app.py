3c7ed989c5933b57ecacfb802858748a0094aaae
Nadhiar Pradana <nadhiar11@gmail.com>
2022-10-31 15:13:06 +0700

    Edit Mail

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,10 +13,10 @@ st.info('Data Enthusiast with an interest in Data Science and Data Visualization
 
 icon_size = 20
 
-st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
-st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
-st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
-st_button('github', 'https://twitter.com/thedataprof/', 'Follow me on Twitter', icon_size)
+#st_button('youtube', 'https://youtube.com/dataprofessor', 'Data Professor YouTube channel', icon_size)
+#st_button('youtube', 'https://youtube.com/codingprofessor', 'Coding Professor YouTube channel', icon_size)
+#st_button('medium', 'https://data-professor.medium.com/', 'Read my Blogs', icon_size)
+st_button('github', 'https://github.com/nrwpradana', 'Check my Github', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/nadhiar-ridho-wahyu-pradana/', 'Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'https://sendfox.com/dataprofessor/', 'Sign up for my Newsletter', icon_size)
-st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
+st_button('newsletter', 'mailto:nadhiar11@gmail.com', 'Mail me', icon_size)
+#st_button('cup', 'https://www.buymeacoffee.com/dataprofessor/', 'Buy me a Coffee', icon_size)
