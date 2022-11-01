2781263f87bd4a51c63014d277df553cb6811c3e
Nadhiar Pradana <nadhiar11@gmail.com>
2022-11-01 09:54:31 +0700

    Edit Button

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@ import streamlit as st
 from st_functions import st_button, load_css
 from PIL import Image
 
+st.set_page_config(page_title='Krisis Pangan 2022', page_icon = "🌽", layout = 'wide', initial_sidebar_state = 'auto')
+
 load_css()
 
 col1, col2, col3 = st.columns(3)
@@ -15,10 +17,3 @@ icon_size = 25
 
 st_button('github', 'https://github.com/nrwpradana', ' Check my Github', icon_size)
 st_button('linkedin', 'https://www.linkedin.com/in/nadhiar-ridho-wahyu-pradana/', ' Follow me on LinkedIn', icon_size)
-st_button('newsletter', 'mailto:nadhiar11@gmail.com', ' Mail me', icon_size)
-st_button('projector', 'https://public.tableau.com/app/profile/nadhiar', ' See my Tableau public', icon_size)
-
-st.info('Check My Latest Project Here')
-
-st_button('coffee','https://nrwpradana-krisis-pangan-app-nqc42o.streamlitapp.com/',' Tetris Capstone Project', icon_size)
-st_button('projector','https://public.tableau.com/app/profile/nadhiar/viz/PenilaianIntegritasProvinsiJawaBarat/PenilaianIntegritasJabar',' Jabar Dataviz Fest 2022', icon_size)
